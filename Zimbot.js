790fb542d01da870f931360cf47ec1a987140941
Ash king <88268317+Ashking92@users.noreply.github.com>
2022-06-06 20:50:43 +0530

    Update Zimbot.js

--- Zimbot.js
+++ Zimbot.js
@@ -1,16 +1,16 @@
 
 
 // ⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈
-//▮ZIM BOT INC 2022 ®️ALL RIGHTS RESERVED
+//SARA BOT INC 2022 ®️ALL RIGHTS RESERVED
 //▮
 //▮FORK AND DON'T FORGET TO GIVE A STAR
 //▮
 //▮THIS SOFTWARE IS UNDER UZ COPYRIGHT
 //▮
 //▮REPORT ABUSE OF THIS SOFTWARE EMAIL US
-//▮reinhardtuna@mail.uk
-//▮WHATSAPP US : +44 7441 437150
-//▮YOUTUBE CHANNELL: https://youtube.com/c/DRIPSOFC
+//▮KINGASH1530@GMAIL.COM 
+//▮WHATSAPP US : +91 73850 66631
+//▮YOUTUBE CHANNELL: https://instagram.com/ash_the_hackerr/
 //▮
 //╰▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
 //
@@ -18,8 +18,8 @@
 //┃THIS SOFTWARE INCLUDES 
 //┃SOME ENCRYPTED FILES
 //┃
-//┃THANKS FOR CHOOSING ZIMBOT
-//┃THANKS TO DIKA ARDNT
+//┃THANKS FOR CHOOSING SARABOT 
+//┃THANKS TO ASH KING
 //┗━━━━━━━━━━━━━━━━━━━━━━━━━
 //
 
